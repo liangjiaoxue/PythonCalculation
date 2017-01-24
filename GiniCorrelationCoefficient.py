c330a8bf597c8a324b1dfd43b017d73b15b9e0ed
Liangjiao Xue <liangjiaoxue@users.noreply.github.com>
2017-01-24 12:49:47 -0500

    Wrap global variables in to a class

--- GiniCorrelationCoefficient.py
+++ GiniCorrelationCoefficient.py
@@ -8,10 +8,10 @@ Calculate Gini Correlation Coefficient (GCC). This speeded up version of GCC on 
 
 numpy, multipleprocessing, pandas modules are imported in this code.
 usage:
-python GiniCorrelationCoefficient.py    matrix  output threadnum
+python GiniCorrelationCoefficient.py    matrix  output   threadnum
 matrix : input matrix file with headline(sample IDs) and row names(gene IDs), each row contains data for each gene.
 output: output file
-threadnum: number of cores to be used
+threadnum: number of cores/threads to be used
 '''
 
 from sys import argv
@@ -22,13 +22,14 @@ import pandas as pd
 
 # Worker in the parallel loop
 def worker(index_in, list_in):
-    global data
-    global data_order
-    global data_self_sum
-    global index2range
-    global gene_num
-    range_in = index2range[index_in]
-    part_num = int(gene_num / thread_num)
+    global MaInfo
+    data = MaInfo.data
+    data_order = MaInfo.data_order
+    data_self_sum = MaInfo.data_self_sum
+    range_in = MaInfo.index2range[index_in]
+    gene_num = MaInfo.gene_num
+    part_num = MaInfo.part_num
+
     #print(range_in)
     array2d =  np.empty((len(range_in),gene_num), float)
     for x_pos in range_in :
@@ -51,53 +52,60 @@ def self_sum (vector) :
     return sum_my
 
 
-# read data into array
-def read_matrix(matrix_in):
-    global data
-    global data_order
-    global data_self_sum
-    global index2range
-    global gene_num
-    with open(matrix_in,"rU") as textFile:
-        lines = [line.rstrip().split("\t") for line in textFile]
-        array = np.array(lines[1:])  # remove head line
-        gene = array[:, 0]
-        data = (array[:, 1:]).astype(np.float)  # remove first column
-        # sort array
-        data_sorted = np.apply_along_axis(np.sort, axis=1, arr=data)
-        data_order = np.apply_along_axis(np.argsort, axis=1, arr=data)
-        ## pre-calculation of sum to avoid repetitive calculation
-        data_self_sum = np.apply_along_axis(self_sum, axis=1, arr=data_sorted)
-        # data partition
-        shape = data.shape
-        gene_num = shape[0]
-        #
-        index2range = {}
-        part_num = int(gene_num/thread_num)
-        for index in range(thread_num) :
-            start_in = index* part_num
-            end_in   = (index+1)* part_num
-            if index == thread_num -1 :
-                end_in = gene_num
-            index2range[index] = range(start_in,end_in)
-        return gene
 
+class MatrixInfo :
+    """
+    To  hold the matrix for GCC calculation.
+    Some statistics values are pre-calculated.
+    """
+    def __init__(self,file_in):
+        self.data =None
+        self.data_order=None
+        self.data_self_sum=None
+        self.index2range = {}
+        self.gene = []
+        self.gene_num = 0
+        self.part_num = 0
+
+        with open(file_in, "rU") as textFile:
+            lines = [line.rstrip().split("\t") for line in textFile]
+            array = np.array(lines[1:])  # remove head line
+            self.gene = array[:, 0]
+            self.data = (array[:, 1:]).astype(np.float)  # remove first column
+            data = self.data
+            # sort array
+            data_sorted = np.apply_along_axis(np.sort, axis=1, arr=data)
+            self.data_order = np.apply_along_axis(np.argsort, axis=1, arr=data)
+            ## pre-calculation of sum to avoid repetitive calculation
+            self.data_self_sum = np.apply_along_axis(self_sum, axis=1, arr=data_sorted)
+            # data partition
+            shape = data.shape
+            self.gene_num = shape[0]
+
+    def split_job(self,thread_num):
+        part_num = int(self.gene_num / thread_num)
+        self.part_num = part_num
+        for index in range(thread_num):
+            start_in = index * part_num
+            end_in = (index + 1) * part_num
+            if index == thread_num - 1:
+                end_in = self.gene_num
+            self.index2range[index] = range(start_in, end_in)
+# End of class
 
 
 #######################################################
 #  Main function
 #######################################################
-data = None
-data_order = None # order data from matrix
-data_self_sum = None  # sum of sorted data to avoid calculation in each loop
-gene_num = 0
-index2range = None # hash file to help parallelisation
-thread_num = None
+
 if __name__ == '__main__':
     code, matrix_in, output_file, thread_num0 = argv
     thread_num = int(thread_num0)
     # read file
-    gene_id =read_matrix(matrix_in)
+    MaInfo = MatrixInfo(matrix_in)
+    MaInfo.split_job(thread_num)
+    gene_id =  MaInfo.gene
+
     # Do parallel
     manager = mp.Manager()
     return_list = manager.list(range(thread_num))
@@ -110,6 +118,7 @@ if __name__ == '__main__':
     for p in jobs:
         p.join()
     # collect data
+    gene_num=MaInfo.gene_num
     out =  np.empty((0,gene_num), float)
     for i in range(thread_num):
         array_out = return_list[i]
